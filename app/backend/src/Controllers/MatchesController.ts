dad017006454c19853ad87d4b80ec08d43e1a542
lcoelho3412 <leandrocoelho3@yahoo.com>
2023-01-17 20:07:14 -0300

    Adds filter to endpoint GET /matches

--- app/backend/src/Controllers/MatchesController.ts
+++ app/backend/src/Controllers/MatchesController.ts
@@ -2,8 +2,15 @@ import { Request, Response } from 'express';
 import MatchesService from '../service/MatchesService';
 
 export default class MatchesController {
-  static async getAllMatches(_req: Request, res: Response) {
-    const matches = await MatchesService.getAllMatches();
+  static async getAllMatches(req: Request, res: Response) {
+    const { inProgress } = req.query;
+
+    let matches;
+    if (inProgress) {
+      matches = await MatchesService.getAllMatches();
+    } else {
+      matches = await MatchesService.getMatchesInProgress(String(inProgress));
+    }
     res.status(200).json(matches);
   }
 }
--- app/backend/src/service/MatchesService.ts
+++ app/backend/src/service/MatchesService.ts
@@ -8,4 +8,13 @@ export default class MatchesService {
       { model: Team, as: 'teamAway', attributes: { exclude: ['id'] } },
     ] });
   }
+
+  static async getMatchesInProgress(param: string) {
+    const inProgress = JSON.parse(param);
+    return Matches.findAll({ where: { inProgress },
+      include: [
+        { model: Team, as: 'teamHome', attributes: { exclude: ['id'] } },
+        { model: Team, as: 'teamAway', attributes: { exclude: ['id'] } },
+      ] });
+  }
 }
