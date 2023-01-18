4863bfd319586a4cc8ecc7e475aa527084732bd9
lcoelho3412 <leandrocoelho3@yahoo.com>
2023-01-17 23:01:25 -0300

    "adds middleware to prevent adding duplicate team"

--- app/backend/src/routes/matchesRoutes.ts
+++ app/backend/src/routes/matchesRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
+import matchValidator from '../middleware/MatchValidMiddleware';
 import MatchesController from '../Controllers/MatchesController';
 
 const router = Router();
 
 router.get('/', MatchesController.getAllMatches);
-router.post('/', MatchesController.createMatch);
+router.post('/', matchValidator, MatchesController.createMatch);
 router.patch('/:id/finish', MatchesController.editMatch);
 
 export default router;
